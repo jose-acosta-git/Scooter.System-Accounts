d9039a488f0dfce4481054f00cb891a3390d3025
jose-acosta-git <jose.acosta.git@gmail.com>
2023-11-01 14:01:50 -0300

    Remove isActive from AccountDto

--- src/main/java/accounts/dtos/AccountDto.java
+++ src/main/java/accounts/dtos/AccountDto.java
@@ -7,14 +7,12 @@ public class AccountDto {
 	private LocalDate registrationDate;
 	private double balance;
 	private String mercadoPagoId;
-	private boolean isActive;
 	
-	public AccountDto(LocalDate registrationDate, double balance, String mercadoPagoId, boolean isActive) {
+	public AccountDto(LocalDate registrationDate, double balance, String mercadoPagoId) {
 		super();
 		this.registrationDate = registrationDate;
 		this.balance = balance;
 		this.mercadoPagoId = mercadoPagoId;
-		this.isActive = isActive;
 	}
 
 	public AccountDto() {}
@@ -22,5 +20,4 @@ public class AccountDto {
 	public LocalDate getRegistrationDate() {return registrationDate;}
 	public double getBalance() {return balance;}
 	public String getMercadoPagoId() {return mercadoPagoId;}
-	public boolean isActive() {return isActive;}
 }
