0b825c5210d75f6413d9a2ca7577bf364c03afc3
jose-acosta-git <jose.acosta.git@gmail.com>
2023-11-18 05:05:40 -0300

    Remove comments

--- src/test/java/accounts/AccountsServiceTests.java
+++ src/test/java/accounts/AccountsServiceTests.java
@@ -3,7 +3,6 @@ package accounts;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cglib.core.Local;
 
 import accounts.dtos.AccountDto;
 import accounts.model.Account;
@@ -23,16 +22,14 @@ public class AccountsServiceTests {
 
     @Test
     void convertToEntity_shouldReturnAccount() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        // Given
+        
         LocalDate today = LocalDate.now();
         AccountDto accountDto = new AccountDto(today, 100.0, "123456");
-
-        // When
+        
         Method convertToEntityMethod = AccountsService.class.getDeclaredMethod("convertToEntity", AccountDto.class);
         convertToEntityMethod.setAccessible(true);  // Make the private method accessible
         Account result = (Account) convertToEntityMethod.invoke(accountsService, accountDto);
 
-        // Then
         assertEquals(today, result.getRegistrationDate());
         assertEquals(100.0, result.getBalance());
         assertEquals("123456", result.getMercadoPagoId());
